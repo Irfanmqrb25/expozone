849f07c8eceef4a05325a58fa27d419ed390f6af
Irfan Muqorib <irfanmuqorib33@gmail.com>
2023-07-24 22:07:45 +0700

    Add cart feature

--- app/featured/page.tsx
+++ app/featured/page.tsx
@@ -5,6 +5,7 @@ import RemoveFilter from "@/components/RemoveFilter";
 import ProductCard from "@/components/product/ProductCard";
 
 import getProducts, { IProductParams } from "@/actions/getProducts";
+import getCurrentUser from "@/lib/session";
 
 interface HomePageProps {
   searchParams: IProductParams;
@@ -12,6 +13,7 @@ interface HomePageProps {
 
 const HomePage = async ({ searchParams }: HomePageProps) => {
   const products = await getProducts(searchParams);
+  const session = await getCurrentUser();
 
   if (products?.length === 0) {
     return (
@@ -25,7 +27,11 @@ const HomePage = async ({ searchParams }: HomePageProps) => {
     <Container>
       <div className="grid grid-cols-1 gap-5 pt-20 mx-1 md:gap-3 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
         {products?.map((product) => (
-          <ProductCard key={product.id} productData={product} />
+          <ProductCard
+            session={session}
+            key={product.id}
+            productData={product}
+          />
         ))}
       </div>
     </Container>
